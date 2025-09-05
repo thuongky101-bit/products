93e84a23565184f3daf92e6f9b09eb76f2d847fc
thuongky101 <thuongky101@gmail.com>
2025-09-04 22:33:11 -0400

    Delete migration_test.js

--- migration_test.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-// Load the storage data from JSON instead of a script module
-const data = JSON.parse(fs.readFileSync('storage.json', 'utf8'));
-
-console.log('Loaded version:', data.version);
-console.log('Preset:', data.settings.preset);
-console.log('Topic count:', data.topics.length);
